30ceff48ecfab93828359f4cee1e446838863174
petiepositivo <pvictoratos@rocketmail.com>
2022-04-28 00:29:48 -0400

    added math
    app will now calculate and display price per person

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -14,9 +14,22 @@ struct ContentView: View {
     @State private var tipPercentage = 20
     let tipPercentages = [10, 15, 20, 25, 0]
     
+    var totalPerPerson: Double {
+        //calculate the total owed per person
+        let peopleCount = Double(numberOfPeople + 2) //accounting for Picker UI
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
         NavigationView {
             Form {
+                //Gather the amount on the bill
                 Section {
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                             .keyboardType(.decimalPad)
@@ -28,6 +41,7 @@ struct ContentView: View {
                     }
                 }
                 
+                //Select the tip percentage
                 Section {
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(tipPercentages, id: \.self) {
@@ -38,8 +52,9 @@ struct ContentView: View {
                     Text("How much tip do you want to leave?")
                 }
                 
+                //See the price per person
                 Section {
-                    Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                 }
             }.navigationTitle("WeSplit 🧾")
         }
