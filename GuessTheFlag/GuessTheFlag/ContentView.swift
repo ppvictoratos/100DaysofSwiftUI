7b9e7ec8a8065c2ecdd259b0940549f67c621259
petiepositivo <pvictoratos@rocketmail.com>
2022-05-11 15:18:57 -0700

    enhanced proj 3
    added a custom view to guess the flag

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -7,6 +7,17 @@
 
 import SwiftUI
 
+struct FlagImage: View {
+    var imgName: String
+    
+    var body: some View {
+        Image(imgName)
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .shadow(radius: 5)
+    }
+}
+
 struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
@@ -44,10 +55,7 @@ struct ContentView: View {
                         Button {
                             flagTapped(number)
                         } label: {
-                            Image(countries[number])
-                                .renderingMode(.original)
-                                .clipShape(Capsule())
-                                .shadow(radius: 5)
+                            FlagImage(imgName: countries[number])
                         }
                     }
                 }
